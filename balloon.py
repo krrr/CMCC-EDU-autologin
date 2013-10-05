02712891f0afe6eeb36a54cff971718173d270ed
kcworms <guogaishiwo@gmail.com>
2013-10-05 12:39:42 +0800

    add time reminding

---
+++ balloon.py
@@ -0,0 +1,28 @@
+"For Windows only.Using PowerShell Script to show balloon in system tray"
+import subprocess
+def show(time):
+    used, total = time
+    ps = '''[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    $objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon
+    $objNotifyIcon.Icon = "D:\\1.ico"
+    $objNotifyIcon.BalloonTipIcon = "Info"
+    $objNotifyIcon.BalloonTipText = "%s/%s."
+    $objNotifyIcon.BalloonTipTitle = "TEST"
+    $objNotifyIcon.Visible = $True
+    $objNotifyIcon.ShowBalloonTip(1000)
+    Sleep 2
+    $objNotifyIcon.Visible = $False
+    exit''' % (used, total)
+
+    subprocess.Popen(['powershell.exe', ps])
+
+
+
+
+
+
+
+
+
+
+
