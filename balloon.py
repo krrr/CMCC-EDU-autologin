2c5f4eafc94bac38833cef25001c492f829969d6
kcworms <guogaishiwo@gmail.com>
2013-10-07 22:10:25 +0800

    completed balloonshower,adding logout

--- balloon.py
+++ balloon.py
@@ -1,17 +1,16 @@
 "For Windows only.Using PowerShell Script to show balloon in system tray"
 import subprocess
-def show(time):
-    used, total = time
+def show(used, total, showtime):
     ps = '''[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     $objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon
     $objNotifyIcon.Icon = "icon.ico"
     $objNotifyIcon.BalloonTipIcon = "Info"
-    $objNotifyIcon.BalloonTipText = "%s/%s."
-    $objNotifyIcon.BalloonTipTitle = "TEST"
+    $objNotifyIcon.BalloonTipText = "%smin (used)/%smin (total)"
+    $objNotifyIcon.BalloonTipTitle = "Time reminder"
     $objNotifyIcon.Visible = $True
     $objNotifyIcon.ShowBalloonTip(1000)
-    Sleep 2
+    Sleep %s
     $objNotifyIcon.Visible = $False
-    exit''' % (used, total)
+    exit''' % (used, total, showtime)
 
     subprocess.Popen(['powershell.exe', ps])
--- t.py
+++ t.py
@@ -9,27 +9,58 @@ passwd = ''
 
 cj = http.cookiejar.LWPCookieJar('cookie.txt')
 opener = urlreq.build_opener(urlreq.HTTPCookieProcessor(cj))
-opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Tablet PC 2.0)')]
+opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 8.0;\
+Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.3072\
+9; .NET CLR 3.0.30729; Tablet PC 2.0)')]
 
-temp = str(opener.open('http://g.cn').read())
-# iURL: for getting lURL,validates,userip,acip... 
-# lURL: for sending username,pass,all.
+temp = str(opener.open('http://8.8.8.8').read())
+# iURL: for getting lURL,validates,userip,acip...
+# lURL: for sending all data to login(HTTP POST).
 iURL = re.search(r'<iframe class="Wp_frame" id="Wp_frame" src="(.+?)" ', temp).group(1)
-lURL = 'https://' + re.search(r'https://(.+?):', iURL).group(1) + ':7090/zmcc/cmcc_edu_do_login.php'
+serverip = re.search(r'https://(.+?):', iURL).group(1)
+lURL = 'https://' + serverip + ':7090/zmcc/cmcc_edu_do_login.php'
 
 userip = re.search(r'wlanuserip=(.+?)&', iURL).group(1)
 acname = re.search(r'wlanacname=(.+?)&', iURL).group(1)
 acip = re.search(r'wlanacip=(.+?)&', iURL).group(1)
 
 temp = str(opener.open(iURL).read())
+print(temp.find('本月套餐已'))
 try:  # get validation code
     hidvad = re.search(r"validateid\\'  value=\\'(.+?)\\'", temp).group(1)
-    vad = hidvad[:4]
     hidvad = hidvad.replace('|', '%7C')
-    data = 'username=%s&password=%s&loginvalidate=%s&loginhiddenvalidate=%s&loginmode=static&wlanacssid=CMCC-EDU&wlanacname=%s&wlanacip=%s&wlanuserip=%s&issaveinfo=' % (username,passwd,vad,hidvad,acname,acip,userip)
+    vad = hidvad[:4]
+    data = 'username=%s&password=%s&loginvalidate=%s&loginhiddenvalidate=%s\
+&loginmode=static&wlanacssid=CMCC-EDU&wlanacname=%s&wlanacip=%s\
+&wlanuserip=%s&issaveinfo=1'\
+    % (username, passwd, vad, hidvad, acname, acip, userip)
 except:  # no validation code
-    data = 'username=%s&password=%s&loginmode=static&wlanacssid=CMCC-EDU&wlanacname=%s&wlanacip=%s&wlanuserip=%s&issaveinfo=' % (username,passwd,acname,acip,userip)
+    data = 'username=%s&password=%s&loginmode=static&wlanacssid=CMCC-EDU\
+&wlanacname=%s&wlanacip=%s&wlanuserip=%s&issaveinfo=1'\
+    % (username, passwd, acname, acip, userip)
 
-temp = str(urlreq.urlopen(lURL, data.encode('utf-8')).read())
+temp = str(opener.open(lURL, data.encode('utf-8')).read())
+cj.save()
 # sURL:for going to a page contains remianing time,logout URL.
 sURL = re.search(r"window.location = \\'(.+?)\\'", temp).group(1)
+if sURL.find('user_status.php?') != -1:
+    oURL ='http://' + serverip + ':7080/zmcc/cmcc_edu_do_logout.php'
+    # using POST
+    temp = str(opener.open(sURL.replace(' ', '%20')).read())
+
+    import balloon
+    ba_re = re.search(r'本月套餐已用：(.+?).0 分钟', temp).group(1)
+    ba_to = re.search(r'本月套餐总量：(.+?).0 分钟', temp).group(1)
+    balloon.show(ba_re, ba_to, 3)
+
+    for i in ['logintime', 'remaintime', 'areacode', 'productid',
+              'effecttime', 'expiretime', 'kestr', 'cf', 'logonsessid']:
+        eval('(i1) = re.search(r"value=(.+?) type=hidden name=(i2)>", temp)'
+             % {i1:i, i2:i})
+    data = 'username=(username)&logintime=(logintime)&remaintime=(remaintime)\
+&areacode=(areacode)&wlanacip=(acip)\&wlanacname=(acname)\
+&wlanacssid=CMCC-EDU&wlanuserip=(userip)&productid=(productid)\
+&effecttime=(effecttime)\&expiretime=(expiretime)&presenttime=\
+&keystr=(keystr)&cf=(cf)&logouttype=TYPESUBMIT\
+&logonsessid=(logonsessid)' % vars()
+    opener.open(oURL, data.encode('utf-8'))
