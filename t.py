45997d90d865e5fa638a424ce1aab491199b97e2
kcworms <guogaishiwo@gmail.com>
2013-10-11 17:05:46 +0800

    rewrited,make it func

--- t.py
+++ t.py
@@ -1,11 +1,6 @@
 ﻿import re
-try:
-    # for Python3
-    import urllib.request as urlreq
-    import http.cookiejar as Cookie
-except ImportError:
-    import urllib2 as urlreq
-    import cookielib as Cookie
+import urllib.request as urlreq
+import http.cookiejar as Cookie
 import sys
 
 try:
@@ -26,66 +21,78 @@ try:
 except:
     print('Error: no network connection or network connected is not CMCC')
     sys.exit()
+
 # iURL: for getting lURL,validates,userip,acip...
 # lURL: for sending all data to login(HTTP POST).
 iURL = re.search(r'<iframe class="Wp_frame" id="Wp_frame" src="(.+?)" ', temp).group(1)
-serverip = re.search(r'https://(.+?):', iURL).group(1)
-lURL = 'https://' + serverip + ':7090/zmcc/cmcc_edu_do_login.php'
+lURL = iURL.partition('cmcc_edu_input.php')[0] + 'cmcc_edu_do_login.php'
 
 userip = re.search(r'wlanuserip=(.+?)&', iURL).group(1)
 acname = re.search(r'wlanacname=(.+?)&', iURL).group(1)
 acip = re.search(r'wlanacip=(.+?)&', iURL).group(1)
 
-temp = str(opener.open(iURL).read())
+def login_noC():
+    temp = str(opener.open(iURL).read())
 
-try:  # get validation code
-    hidvad = re.search(r"validateid\\'  value=\\'(.+?)\\'", temp).group(1)
-    hidvad = hidvad.replace('|', '%7C')
-    vad = hidvad[:4]
-    data = 'username=%s&password=%s&loginvalidate=%s&loginhiddenvalidate=%s\
+    try:  # get validation code
+        hidvad = re.search(r"validateid\\'  value=\\'(.+?)\\'", temp).group(1)
+        hidvad = hidvad.replace('|', '%7C')
+        vad = hidvad[:4]
+        data = 'username=%s&password=%s&loginvalidate=%s&loginhiddenvalidate=%s\
 &loginmode=static&wlanacssid=CMCC-EDU&wlanacname=%s&wlanacip=%s\
 &wlanuserip=%s&issaveinfo=1'\
-    % (username, passwd, vad, hidvad, acname, acip, userip)
-except:  # no validation code
-    data = 'username=%s&password=%s&loginmode=static&wlanacssid=CMCC-EDU\
+        % (username, passwd, vad, hidvad, acname, acip, userip)
+    except:  # no validation code
+        data = 'username=%s&password=%s&loginmode=static&wlanacssid=CMCC-EDU\
 &wlanacname=%s&wlanacip=%s&wlanuserip=%s&issaveinfo=1'\
-    % (username, passwd, acname, acip, userip)
+        % (username, passwd, acname, acip, userip)
 
-temp = str(opener.open(lURL, data.encode('utf-8')).read())
-# sURL:for going to a page contains remianing time,logout URL.
-sURL = re.search(r"window.location = \\'(.+?)\\'", temp).group(1)
-if sURL.find('user_status.php?') != -1:
-    print('Info: login succeed')
-    oURL ='http://' + serverip + ':7080/zmcc/cmcc_edu_do_logout.php'
-    temp = opener.open(sURL.replace(' ', '%20')).read()
-    temp = temp.decode('gbk')
+    lPAGE = str(opener.open(lURL, data.encode('utf-8')).read())
+    return checker(lPAGE)
 
-    if 'remind' in sys.argv:
-        import balloon
-        icopath = sys.argv[0].replace('t.py', 'icon.ico')
-        ba_re = re.search(r'本月套餐已用：(.+?).0 分钟', temp).group(1)
-        ba_to = re.search(r'本月套餐总量：(.+?).0 分钟', temp).group(1)
-        balloon.show(icopath, ba_re, ba_to, 4)
 
-#    logout seems no need anymore
-#    def logout(oURL, oURLpage):
-#        datadic = {}
-#        for i in ['logintime', 'remaintime', 'areacode', 'productid',
-#                  'effecttime', 'expiretime', 'keystr', 'cf', 'logonsessid']:
-#            try:
-#                datadic[i] = re.search(r"<input type='hidden' name='%s'(\s+?)value='(.+?)'>" % i, oURLpage).group(2)
-#            except AttributeError:
-#                datadic[i] = ''
-#
-#        datadic.update(vars())
-#        data = 'username=%(username)s&logintime=%(logintime)s&remaintime=%(remaintime)s\
-#&areacode=%(areacode)s&wlanacip=%(acip)s&wlanacname=%(acname)s\
-#&wlanacssid=CMCC-EDU&wlanuserip=%(userip)s&productid=%(productid)s\
-#&effecttime=%(effecttime)s&expiretime=%(expiretime)s&presenttime=\
-#&keystr=%(keystr)s&cf=%(cf)s&logouttype=TYPESUBMIT\
-#&logonsessid=%(logonsessid)s' % datadic
-#        data = urlpar.quote_plus(data, safe='=&')
-#        opener.open(oURL, data.encode('utf-8'))
+def login_C():
+    cookiejar.load()
+    data = 'wlanacname=%s&wlanacip=%s&wlanuserip=%s&wlanacssid=CMCC-EDU\
+&loginmode=cookie' % (acname, acip, userip)
 
-else:
-    print('Error: login failed')
+    lPAGE = str(opener.open(lURL, data.encode('utf-8')).read())
+    return checker(lPAGE)
+
+def checker(lPAGE):
+    if lPAGE.find('user_status.php?') != -1:
+        print('Info: login succeed')
+        savecookie(lPAGE,lURL)
+        # Time Reminder
+        if 'remind' in sys.argv:
+            import balloon
+            # sURL:for going to a page contains remianing time,logout URL.
+            sURL = re.search(r"window.location = \\'(.+?)\\'", lPAGE).group(1)
+            temp = opener.open(sURL.replace(' ', '%20')).read()
+            temp = temp.decode('gbk')
+            icopath = sys.argv[0].replace('t.py', 'icon.ico')
+            ba_re = re.search(r'本月套餐已用：(.+?).0 分钟', temp).group(1)
+            ba_to = re.search(r'本月套餐总量：(.+?).0 分钟', temp).group(1)
+            balloon.show(icopath, ba_re, ba_to, 4)
+        return 'Succeed'
+    else:
+        print('Error: login failed')
+
+def savecookie(lPAGE,lURL):
+    import urllib.parse as urlpar
+    cookielst = re.findall(r'setCookie\("(.+)",\s{0,}"(.+)?",\s?.?365.?\)', lPAGE)
+    cookiedic = {i[0]: urlpar.quote(i[1], safe='=&') for i in cookielst}
+    cookiedic['supportCookie'] = 'YES'
+
+    lURL = lURL.replace('https://', '')
+    serverip = lURL.partition(':')[0]
+    path = '/' + lURL.partition('/')[2]
+    with open('cookie.txt', 'w') as cookiefile:
+        cookiefile.write('#LWP-Cookies-2.0\n')
+        for i in cookiedic:
+            cookiefile.write('Set-Cookie3: %s=%s; path="%s";\
+domain="%s"; path_spec; expires="2015-10-10 05:17:57Z";\
+httponly=None;version=0\n' % (i, cookiedic[i], path, serverip))
+
+if __name__ == '__main__':
+    login_noC()
