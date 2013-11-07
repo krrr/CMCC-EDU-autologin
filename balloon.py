399db310698ed90f917419b04fbec5038cd135af
kcworms <guogaishiwo@gmail.com>
2013-11-07 21:50:02 +0800

    add some instruments

--- balloon.py
+++ balloon.py
@@ -1,4 +1,8 @@
-# Modified from Pyzen and  gingerprawn.The license of gingerprawn is GPLv3.
+"""Display balloon tip on Windows.
+This module provides a function that display a tray icon with balloon tip
+by calling winapi directly."""
+
+# Modified from Pyzen and gingerprawn.The license of gingerprawn is GPLv3.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,13 +15,16 @@
 # GNU General Public License for more details.
 
 
+
 from ctypes import *
 from ctypes.wintypes import *
 from time import sleep
 
-# reference
-# http://msdn.microsoft.com/en-us/library/windows/desktop/ms648045%28v=vs.85%29.aspx (LoadImage function)
-# http://msdn.microsoft.com/en-us/library/windows/desktop/bb773352%28v=vs.85%29.aspx (NOTIFYICONDATA structure)
+#References
+#LoadImage function:
+#http://msdn.microsoft.com/en-us/library/windows/desktop/ms648045%28v=vs.85%29.aspx
+#NOTIFYICONDATA structure:
+#http://msdn.microsoft.com/en-us/library/windows/desktop/bb773352%28v=vs.85%29.aspx
 
 Shell_NotifyIcon = windll.shell32.Shell_NotifyIconW
 LoadImage = windll.user32.LoadImageW
@@ -25,56 +32,57 @@ LoadImage = windll.user32.LoadImageW
 
 class GUID(Structure):
     _fields_ = [
-            ('Data1', c_uint32),
-            ('Data2', c_uint16),
-            ('Data3', c_uint16),
-            ('Data4', c_ubyte * 8),
-            ]
+        ('Data1', c_uint32),
+        ('Data2', c_uint16),
+        ('Data3', c_uint16),
+        ('Data4', c_ubyte * 8),
+        ]
 
 
 class NOTIFYICONDATA(Structure):
     _fields_ = [
-            ('cbSize', DWORD),
-            ('hWnd', HWND),
-            ('uID', UINT),
-            ('uFlags', UINT),
-            ('uCallbackMessage', UINT),
-            ('hIcon', HICON),
-            ('szTip', c_wchar * 128),
-            ('dwState', DWORD),
-            ('dwStateMask', DWORD),
-            ('szInfo', c_wchar * 256),
-            ('uTimeout', UINT), # Also is uVersion
-            ('szInfoTitle', c_wchar * 64),
-            ('dwInfoFlags', DWORD),
-            ('guidItem', GUID),
-            ('hBalloonIcon', HICON),
-            ]
+        ('cbSize', DWORD),
+        ('hWnd', HWND),
+        ('uID', UINT),
+        ('uFlags', UINT),
+        ('uCallbackMessage', UINT),
+        ('hIcon', HICON),
+        ('szTip', c_wchar * 128),
+        ('dwState', DWORD),
+        ('dwStateMask', DWORD),
+        ('szInfo', c_wchar * 256),
+        ('uTimeout', UINT),  # Also is uVersion
+        ('szInfoTitle', c_wchar * 64),
+        ('dwInfoFlags', DWORD),
+        ('guidItem', GUID),
+        ('hBalloonIcon', HICON),
+        ]
 
-tiptypedic = {'info':1, 'error':3}
+tiptypedic = {'noico': 0, 'info': 1, 'warning': 2, 'error': 3}
+
 
 def show(icopath, title, body, tiptype='info', timeout=5, winver=6):
     icopath = icopath.replace('\\', '\\\\')
 
-    if winver >= 6: # Windows Vista and later
+    if winver >= 6:  # Windows Vista and later
         cbsize = sizeof(NOTIFYICONDATA)
-    else: # Ingore lower OS than Windows XP
+    else:            # Ignore version lower than XP
         cbsize = 952
 
     nid = NOTIFYICONDATA(
-            cbSize=cbsize,
-            hWnd=0,
-            uID=0,
-            uFlags=2|16|128, #NIF_ICON | NIF_INFO | NIF_SHOWTIP
-            szInfo=body,
-            szInfoTitle=title,
-            hIcon=LoadImage(None, icopath, 1, 16, 16, 0x10),
-            dwInfoFlags=tiptypedic[tiptype]
-            )
-    Shell_NotifyIcon(0, byref(nid)) # NIM_ADD
+        cbSize=cbsize,
+        hWnd=0,
+        uID=0,
+        uFlags=2|16|128,  # NIF_ICON | NIF_INFO | NIF_SHOWTIP
+        szInfo=body,
+        szInfoTitle=title,
+        hIcon=LoadImage(None, icopath, 1, 16, 16, 0x10),
+        dwInfoFlags=tiptypedic[tiptype]
+        )
+    Shell_NotifyIcon(0, byref(nid))  # NIM_ADD
     sleep(timeout)
-    Shell_NotifyIcon(2, byref(NOTIFYICONDATA(hWnd=0, uID=0))) # NIM_DELETE
+    Shell_NotifyIcon(2, byref(NOTIFYICONDATA(hWnd=0, uID=0)))  # NIM_DELETE
 
 if __name__ == '__main__':
-    show('', 'Title', 'Body','error', timeout=1)
+    show('', 'Title', 'Body', 'error', timeout=1)
     show('', 'Title', 'Body', timeout=1)
