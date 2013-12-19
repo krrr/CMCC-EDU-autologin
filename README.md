c73cc6561ea48017dc925942f05d98143a640b5d
kcworms <guogaishiwo@gmail.com>
2013-12-19 18:03:56 +0800

    no more mainfunc,add error types

--- README.md
+++ README.md
@@ -9,20 +9,19 @@ It works in a university located in Jinhua,Zhejiang Province,China.
 **(Require Python3)/(只能工作在Python3下)**
 Usage/使用方法
 ----
-Edit config.ini first.Run t.py for console mode,
-or run t_gui.pyw to enable GUI mode(Windows only).
+Edit config.ini first.Then run t.py .
+Use pythonw.exe to run t.py if GUI mode enabled(Windows only).
 
 Cookie will be saved when successfully logged in the first time.When failed logging in by cookie,
 it will fallback to no-cookie login method.
 
-----
-先编辑config.ini文件，然后运行t.py（命令行模式）或者t_gui.pyw（GUI模式，Windows限定）。
 
+先编辑config.ini文件，然后运行t.py。如果启用了GUI模式，（Windows限定）用pythonw来运行t.py。
 第一次成功登陆时会保存Cookie。当用Cookie登录失败时会使用无Cookie的登录方式。
 
-----
+
 ----
 Using the Scheduled Task of Windows,it can run automatically when connected to CMCC-EDU:[HOWTO](http://superuser.com/questions/262799/how-to-launch-a-command-on-network-connection-disconnection).
-But auto-run can easily fail due to a bad connection.
 
-借助Windows的计划任务功能，可以在成功连接至CMCC-EDU后自动运行来登录。但是由于网络连接质量不确定，它工作得并不好。
+
+借助Windows的计划任务功能，可以在成功连接至CMCC-EDU后自动运行来登录。
--- t.py
+++ t.py
@@ -1,3 +1,171 @@
-from mainfunc import main
+﻿import re
+import urllib.request as Urlreq
+import http.cookiejar as Cookie
+import sys
+import os
+import configparser
 
-main()
+
+if os.sep not in sys.argv[0]:  # Run as exe
+    path = ''
+else:
+    path = os.path.split(sys.argv[0])[0] + os.sep
+
+try:
+    config = configparser.ConfigParser()
+    config.read(path + 'config.ini')
+    username = config['Main']['Username']
+    passwd = config['Main']['Password']
+    timereminder_on = config['Main'].getboolean('Timereminder')
+    
+    # set up GUI 
+    if config['GUI']['Type'] == 'Win':
+        import balloon
+        def myprint(dat):
+            if 'Info' in dat: return None
+    
+            if 'Error' in dat or 'fail' in dat:
+                tiptype = 'error'
+            elif 'Warning' in dat:
+                tiptype = 'warning'
+            else:
+                tiptype = 'info'
+
+            dat = dat.split(': ')[-1]
+
+            balloon.show(path + 'icon.ico',
+                        'Cxx-autologin',
+                        dat,
+                        tiptype=tiptype,
+                        timeout=int(config['GUI']['Balloontimeout']),
+                        winver=sys.getwindowsversion().major)
+    #elif config['GUI']['Type'] == 'KDE':
+    #    def myprint(info):
+    #        timeout = int(config['GUI']['Balloontimeout'])
+    #        os.popen("kdialog --passivepopup 'Cxx-autologin\n\n%s' %s" % (info, timeout))
+    else:
+        myprint = print
+except:
+    print('Error: Failed loading config file')
+    sys.exit()
+
+cookiejar = Cookie.LWPCookieJar(path + 'cookie.txt')
+opener = Urlreq.build_opener(Urlreq.HTTPCookieProcessor(cookiejar))
+opener.addheaders = [('User-agent', ('Mozilla/4.0 (compatible; MSIE 8.0; '
+'Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.3072'
+'9; .NET CLR 3.0.30729; Tablet PC 2.0)'))]
+
+try:
+    temp = str(opener.open('http://8.8.8.8', timeout=6).read())
+except:
+    try:
+        temp = opener.open('http://www.google.cn/favicon.ico', timeout=5)
+    except:  # no internet connection
+        myprint('Error: network connection is bad or network connected is not CMCC')
+    finally:
+        sys.exit()
+
+# iURL: for getting lURL,validates,userip,acip...
+# lURL: for sending all data to login(HTTP POST).
+iURL = re.search(r'="Wp_frame" id="Wp_frame" src="(.+?)" ', temp).group(1)
+lURL = iURL.partition('cmcc_edu_input.php')[0] + 'cmcc_edu_do_login.php'
+
+userip = re.search(r'wlanuserip=(.+?)&', iURL).group(1)
+acname = re.search(r'wlanacname=(.+?)&', iURL).group(1)
+acip = re.search(r'wlanacip=(.+?)&', iURL).group(1)
+
+
+def login_noc():
+    ipage = str(opener.open(iURL).read())
+
+    try:  # get validation code
+        hidvad = re.search(r"validateid\\'  value=\\'(.+?)\\'", ipage).group(1)
+        hidvad = hidvad.replace('|', '%7C')
+        vad = hidvad[:4]
+        data = ('username=%s&password=%s&loginvalidate=%s&loginhiddenvalidate=%s'
+                '&loginmode=static&wlanacssid=CMCC-EDU&wlanacname=%s&wlanacip=%s'
+                '&wlanuserip=%s&issaveinfo=1') \
+                    % (username, passwd, vad, hidvad, acname, acip, userip)
+    except:  # no validation code
+        data = ('username=%s&password=%s&loginmode=static&wlanacssid=CMCC-EDU'
+                '&wlanacname=%s&wlanacip=%s&wlanuserip=%s&issaveinfo=1') \
+                    % (username, passwd, acname, acip, userip)
+
+    try:
+        lpage = opener.open(lURL, data.encode('utf-8'), timeout=6).read()
+    except:
+        myprint('Error: Timeout,failed talking with their server')
+        sys.exit()
+    return checker(lpage.decode('gbk'), False)
+
+
+def login_c():
+    cookiejar.load()
+    data = ('wlanacname=%s&wlanacip=%s&wlanuserip=%s&wlanacssid=CMCC-EDU'
+            '&loginmode=cookie') % (acname, acip, userip)
+    try:
+        lpage = opener.open(lURL, data.encode('utf-8'), timeout=6).read()
+    except:
+        myprint('Error: Timeout,failed talking with their server')
+        sys.exit()
+    return checker(lpage.decode('gbk'), True)
+
+
+def checker(lpage, is_c):
+    if 'user_status.php?' in lpage:
+        # skip saving cookie when logged in by cookie
+        if not is_c: savecookie(lpage, lURL)
+        if timereminder_on:  # Time Reminder
+            # sURL:for going to a page contains remianing time,logout URL.
+            surl = re.search(r"window.location = '(.+?)';", lpage).group(1)
+            try:
+                spage = opener.open(surl.replace(' ', '%20')).read()
+                spage = spage.decode('gbk')
+                ba_re = re.search(r'本月套餐已用：(.+?).0 分钟', spage).group(1)
+                ba_to = re.search(r'本月套餐总量：(.+?).0 分钟', spage).group(1)
+                myprint('Time Usage: %s/%s (min)  [used/total]' % (ba_re,ba_to))
+            except:
+                myprint('Login succeed(Failed getting time usage)')
+        else:
+            myprint('Login succeed')
+            
+        return 'Succeed'
+    else:
+        if '认证信息无效' in lpage:
+            myprint('Warning: CookieFailed')
+            return 'CookieFail'
+        else:
+            edic = {'与在线用户名不一致': 'Error: We have not been logged out',
+                    '密码错误': 'Error: Wrong username or password'}
+            for e in edic:
+                if e in lpage:
+                    myprint(edic[e])
+                    return 'Failed'
+
+def savecookie(lpage, lurl):
+    """Parse javascript in lpage and save the cookie to cookie.txt"""
+    import urllib.parse
+
+    cookielst = re.findall(r'setCookie\("(.+)",\s*"(.+)?",\s?.?365.?\)', lpage)
+    cookiedic = {i[0]: urllib.parse.quote(i[1], safe='=&') for i in cookielst}
+    cookiedic['supportCookie'] = 'YES'
+
+    lurl = lurl.replace('https://', '')
+    serverip = lurl.partition(':')[0]
+    cpath = '/' + lurl.partition('/')[2]
+
+    cookiefile = open(path + 'cookie.txt', 'w')
+    cookiefile.write('#LWP-Cookies-2.0\n')
+    for i in cookiedic:
+        cookiefile.write(('Set-Cookie3: %s=%s; path="%s"; '
+        'domain="%s"; path_spec; expires="2017-10-10 05:17:57Z"; '
+        'httponly=None; version=0\n') % (i, cookiedic[i], cpath, serverip))
+    cookiefile.close()
+
+try:
+    cookiejar.load()
+    myprint('Info: Using Cookie')
+except:
+    login_noc()
+else:
+    if login_c() == 'CookieFail': login_noc()
